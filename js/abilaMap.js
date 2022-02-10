a167362211602b6f68e8e598e15c70d1df4da1c7
miacollymoreabbas <mcollymoreabbas@wustl.edu>
2022-02-10 17:28:05 -0600

    quickie clean

--- js/abilaMap.js
+++ js/abilaMap.js
@@ -61,22 +61,6 @@ AbilaMap.prototype.init = function () {
 
     });
 
-    // example append
-    // var g = self.svg.append("g");
-    // // .attr('x', 0)
-    // // .attr('y', 0)
-    // // .attr('width', 20)
-    // // .attr('height', 10);
-    // var projection = d3.geoAlbers();
-    // var path = d3.geoPath(projection);
-    // console.log(path)
-
-    // d3.json("../data/Geospatial/Kronos_Island.json", function (json) {
-    //     g.selectAll("path")
-    //         .data(json.features).enter().append("path").attr("d", path);
-    // })
-
-
 }
 AbilaMap.prototype.changeTime = function (dates) {
     var self = this;
@@ -88,17 +72,34 @@ AbilaMap.prototype.changeTime = function (dates) {
     var currData = new Map(self.gpsData);
     for (var [key, valArr] of currData.entries()) {
         valArr.filter(function (d) {
-            if(d.Timestamp.valueOf() > startDate.valueOf() && d.Timestamp.valueOf() < endDate.valueOf()){
+            if (d.Timestamp.valueOf() > startDate.valueOf() && d.Timestamp.valueOf() < endDate.valueOf()) {
                 gpsArr.push(d)
                 return d;
             }
         });
     }
+
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip-donut")
+        .style("opacity", 0);
     var projection = d3.geoMercator().translate([self.svgWidth / 2, self.svgHeight / 2]).fitSize([self.svgWidth - 5, self.svgHeight - 5], self.topo)
 
     self.svg.selectAll("circle").data(gpsArr).enter().append('circle')
         .attr('cx', function (d) { return projection([d.long, d.lat])[0] })
         .attr('cy', function (d) { return projection([d.long, d.lat])[1] })
         .attr('r', 4).attr('fill', 'red')
+        .on("mouseover", function (event, d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div.html(d.FirstName + " " + d.LastName)
+                .style("left", (event.pageX) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on("mouseout", function (d) {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
 
 }
--- js/kronosMap.js
+++ js/kronosMap.js
@@ -17,18 +17,14 @@ KronosMap.prototype.init = function () {
     self.svgWidth = $(self.svgID).width();
     self.svgHeight = $(self.svgID).height();
 
-    // .center([0, 30])
-    // .translate([self.svgWidth / 2 - self.margin.left, self.svgHeight / 2]);;
 
     var promises = []
     promises.push(d3.json("../data/Geospatial/Kronos_Island.json"))
-    // promises.push(d3.json("../data/Geospatial/Abila.json"))
     myDataPromises = Promise.all(promises).then(function (my_data) {
 
 
         self.topo = my_data[0]
         var projection = d3.geoMercator().translate([self.svgWidth / 2, self.svgHeight / 2]).fitSize([self.svgWidth - 5, self.svgHeight - 5], self.topo)
-        // .fitExtent([0, 0], [self.svgWidth, self.svgHeight], topo);
         var g = self.svg.append("g").attr("width", self.svgWidth)
             .attr("height", self.svgHeight);
 
@@ -76,21 +72,41 @@ KronosMap.prototype.changeTime = function (dates) {
     // should times be an array of the range of Dates...?
     var currData = new Map(self.gpsData);
     for (var [key, valArr] of currData.entries()) {
-        valArr.filter(function (d) {
-            if(d.Timestamp.valueOf() > startDate.valueOf() && d.Timestamp.valueOf() < endDate.valueOf()){
+        var temp = valArr.filter(function (d) {
+            if (d.Timestamp.valueOf() > startDate.valueOf() && d.Timestamp.valueOf() < endDate.valueOf()) {
                 gpsArr.push(d)
                 return d;
             }
         });
+        if (temp.length > 0) {
+            currData.set(key, temp);
+        } else { currData.delete(key) }
     }
     var projection = d3.geoMercator().translate([self.svgWidth / 2, self.svgHeight / 2]).fitSize([self.svgWidth - 5, self.svgHeight - 5], self.topo)
 
-    console.log(gpsArr)
+    console.log(currData)
+
+    var div = d3.select("body").append("div")
+        .attr("class", "tooltip-donut")
+        .style("opacity", 0);
 
     self.svg.selectAll("circle").data(gpsArr).enter().append('circle')
         .attr('cx', function (d) { return projection([d.long, d.lat])[0] })
         .attr('cy', function (d) { return projection([d.long, d.lat])[1] })
         .attr('r', 4).attr('fill', 'red')
+        .on("mouseover", function (event, d) {
+            div.transition()
+                .duration(200)
+                .style("opacity", .9);
+            div.html(d.FirstName + " " + d.LastName)
+                .style("left", (event.pageX) + "px")
+                .style("top", (event.pageY - 28) + "px");
+        })
+        .on("mouseout", function (d) {
+            div.transition()
+                .duration(500)
+                .style("opacity", 0);
+        });
 
 
 
