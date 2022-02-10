4e91a86ffee241d1583e193f626c995b28d20851
miacollymoreabbas <mcollymoreabbas@wustl.edu>
2022-02-10 16:41:00 -0600

    properly populating points

--- js/abilaMap.js
+++ js/abilaMap.js
@@ -83,24 +83,22 @@ AbilaMap.prototype.changeTime = function (dates) {
     self.svg.selectAll('circle').remove();
     var startDate = dates[0];
     var endDate = dates[1];
+    var gpsArr = [];
     // should times be an array of the range of Dates...?
     var currData = new Map(self.gpsData);
     for (var [key, valArr] of currData.entries()) {
-        var tempArr = valArr.filter(d => d.Timestamp.valueOf() > startDate.valueOf() && d.Timestamp.valueOf() < endDate.valueOf());
-        if (tempArr.length > 0) {
-            currData.set(key, tempArr);
-        } else {
-            currData.delete(key);
-        }
+        valArr.filter(function (d) {
+            if(d.Timestamp.valueOf() > startDate.valueOf() && d.Timestamp.valueOf() < endDate.valueOf()){
+                gpsArr.push(d)
+                return d;
+            }
+        });
     }
     var projection = d3.geoMercator().translate([self.svgWidth / 2, self.svgHeight / 2]).fitSize([self.svgWidth - 5, self.svgHeight - 5], self.topo)
 
-    for (var [key, valArr] of currData.entries()) {
-        self.svg.selectAll("circle").data(valArr).enter()
-            .append('circle').attr('r', 4).attr('cx', function (d) { return projection([d.long, d.lat])[0] }).attr('cy', function (d) { return projection([d.long, d.lat])[1] })
-            .attr('fill', 'red')
-
-    }
-    self.svg.selectAll('circle').exit();
+    self.svg.selectAll("circle").data(gpsArr).enter().append('circle')
+        .attr('cx', function (d) { return projection([d.long, d.lat])[0] })
+        .attr('cy', function (d) { return projection([d.long, d.lat])[1] })
+        .attr('r', 4).attr('fill', 'red')
 
 }
