773677067b316cd19fcdf59c3831fd68906fa4d0
miacollymoreabbas <mcollymoreabbas@wustl.edu>
2022-02-24 04:53:51 -0600

    beepity boop

--- js/abilaMap.js
+++ js/abilaMap.js
@@ -61,40 +61,116 @@ AbilaMap.prototype.changeTime = function (dates) {
     self.svg.selectAll('circle').remove();
     var startDate = dates[0];
     var endDate = dates[1];
-    var gpsArr = [];
+    // var gpsArr = [];
     var currData = new Map(self.gpsData);
     for (var [key, valArr] of currData.entries()) {
-        valArr.filter(function (d) {
+        var temp = valArr.filter(function (d) {
             if (d.Timestamp.valueOf() > startDate.valueOf() && d.Timestamp.valueOf() < endDate.valueOf()) {
-                gpsArr.push(d)
                 return d;
             }
         });
+        if (temp.length > 0) {
+            currData.set(key, temp);
+        } else { currData.delete(key) }
     }
+
+    var j = 0;
 
     var div = d3.select("body").append("div")
         .attr("class", "tooltip-donut")
         .style("opacity", 0);
     var projection = d3.geoMercator().translate([self.svgWidth / 2, self.svgHeight / 2]).fitSize([self.svgWidth - 5, self.svgHeight - 5], self.topo)
 
-    self.svg.selectAll("circle").data(gpsArr).enter().append('circle')
-        .attr('cx', function (d) { return projection([d.long, d.lat])[0] })
-        .attr('cy', function (d) { return projection([d.long, d.lat])[1] })
-        .attr('r', 4).attr('fill', 'red')
-        .attr('stroke', 'black')
-        .attr('stroke-width', .5)
-        .on("mouseover", function (event, d) {
-            div.transition()
-                .duration(200)
-                .style("opacity", .9);
-            div.html(d.FirstName + " " + d.LastName)
-                .style("left", (event.pageX) + "px")
-                .style("top", (event.pageY - 28) + "px");
-        })
-        .on("mouseout", function (d) {
-            div.transition()
-                .duration(500)
-                .style("opacity", 0);
-        });
+    var iterator = startDate.valueOf();
+
+    var interval = setInterval(function () {
+        if (iterator < endDate.valueOf()) {
+            self.svg.selectAll('circle').remove();
+            var pointsArr = [];
+
+            var plottedPoints = new Map(currData);
+            console.log(plottedPoints)
+
+            for (var [key, valArr] of plottedPoints.entries()) {
+                var currVals = valArr.filter(function (d) {
+                    if (d.Timestamp.valueOf() >= iterator && d.Timestamp.valueOf() <= (iterator + 1800000)) {
+                        return d;
+                    }
+                });
+                if (currVals.length > 0) {
+                    plottedPoints.set(key, currVals);
+                } else { plottedPoints.delete(key) }
+            }
+            console.log(plottedPoints)
+
+            for (var [key, valArr] of plottedPoints.entries()) {
+                var lat, long = 0.0;
+                valArr.forEach(function (d, i) {
+                    if (i == 0) {
+                        pointsArr.push(d);
+                        lat = d.lat;
+                        long = d.long;
+                    } else {
+                        lat = (lat + d.lat) / 2;
+                        long = (long + d.long) / 2;
+                    }
+                })
+                pointsArr[pointsArr.length - 1].lat = lat;
+                pointsArr[pointsArr.length - 1].long = long;
+
+            }
+            console.log(pointsArr)
+
+
+            var coeff = 1000 * 60 * 5;
+
+
+            var currDate = new Date(iterator)
+            console.log(currDate)
+            document.getElementById("dateTimer").innerHTML = currDate.toLocaleString('en-US', {timezone: 'CST'});
+
+
+
+
+
+            var dots = self.svg.selectAll("circle").data(pointsArr).enter().append('circle')
+                .attr('cx', function (d) { return projection([d.long, d.lat])[0] })
+                .attr('cy', function (d) { return projection([d.long, d.lat])[1] })
+                .attr('r', 4).attr('fill', 'red')
+                .attr('stroke', 'black')
+                .attr('stroke-width', .5)
+                .on("mouseover", function (event, d) {
+                    div.transition()
+                        .duration(200)
+                        .style("opacity", .9);
+                    div.html(d.FirstName + " " + d.LastName)
+                        .style("left", (event.pageX) + "px")
+                        .style("top", (event.pageY - 28) + "px");
+                })
+                .on("mouseout", function (d) {
+                    div.transition()
+                        .duration(500)
+                        .style("opacity", 0);
+                });
+
+
+
+            iterator = iterator + 1800000;
+
+            document.getElementById("pauseButton").addEventListener("click", function(){
+                clearInterval(interval);
+            });
+
+            dots.exit().remove();
+
+        }
+
+    }, 2000);
+
+
+
+    // console.log(gpsArr)
+
+
 
 }
